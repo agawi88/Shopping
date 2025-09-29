3fcd028f340fdb125f8bd369eed9d75dfe4779f2
agawi88 <agnessn@icloud.com>
2025-09-29 14:34:46 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// import the screens
+import ShoppingLists from "./components/ShoppingLists";
+
+const App = () => {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBihgTR-xnxRYcW6TEy9IdpReCX_7AstBg",
+  authDomain: "agas-shopping-list-demo.firebaseapp.com",
+  projectId: "agas-shopping-list-demo",
+  storageBucket: "agas-shopping-list-demo.firebasestorage.app",
+  messagingSenderId: "326283031177",
+  appId: "1:326283031177:web:614a2b367bbe8f39294c58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ShoppingLists"
+      >
+        <Stack.Screen
+          name="ShoppingLists"
+          children={props => <ShoppingLists db={db} {...props} />}
+/>
+{/*           {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
